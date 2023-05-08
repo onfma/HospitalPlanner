555df71ac361f37266daa0eec77a9ce141a0f89c
Maria Onofrei <94899775+onfma@users.noreply.github.com>
2023-05-08 16:46:09 +0300

    Add files via upload

---
+++ src/main/java/com/example/hospitalplanner/HospitalPlannerApplication.java
@@ -0,0 +1,22 @@
+package com.example.hospitalplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HospitalPlannerApplication {
+//http://localhost:9087/
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalPlannerApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+}
---
+++ src/test/java/com/example/hospitalplanner/HospitalPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hospitalplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
