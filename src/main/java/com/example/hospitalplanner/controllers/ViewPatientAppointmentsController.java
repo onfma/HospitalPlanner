81bc315a1037b7373d21c7ca1b37a39865eb684f
claudiuchichirau <chichirau.claudiu@yahoo.ro>
2023-05-18 23:13:46 +0300

    chestii

--- src/main/java/com/example/hospitalplanner/controllers/ViewPatientAppointmentsController.java
+++ src/main/java/com/example/hospitalplanner/controllers/ViewPatientAppointmentsController.java
@@ -6,16 +6,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
-@RequestMapping("/patientViewAccount")
-public class ViewPatientAccountController {
+@RequestMapping("/myAppointments")
+public class ViewPatientAppointmentsController {
     @Autowired
     private HttpSession session;
 
     @GetMapping
-    public String showLoginPage() {
-        System.out.println("S-a afisat pagina patientViewAccount.hmtl!");
+    public String showMyAppointmentsPage() {
+        System.out.println("S-a afisat pagina patientAppointments.hmtl!");
         return "redirect:/"; // redirect to homepage
     }
 }
