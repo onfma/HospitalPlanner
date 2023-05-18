9d24d4251f8e928a0c5edf52c554ac95af6796a7
claudiuchichirau <chichirau.claudiu@yahoo.ro>
2023-05-19 00:29:46 +0300

    return doctors json to patientDashboard

--- src/main/java/com/example/hospitalplanner/controllers/PatientHomePageController.java
+++ src/main/java/com/example/hospitalplanner/controllers/PatientHomePageController.java
@@ -1,10 +1,19 @@
 package com.example.hospitalplanner.controllers;
 
+import ch.qos.logback.core.model.Model;
+import com.example.hospitalplanner.database.DAO.DoctorDAO;
+import com.example.hospitalplanner.database.DAOFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.sql.SQLException;
 
 
 @Controller
@@ -14,11 +23,17 @@ public class PatientHomePageController {
     private HttpSession session;
 
     @GetMapping
-    public String showLoginPage() {
-        // Access the session information
-        String email = (String) session.getAttribute("email");
+    public ModelAndView showPatientDashboardPage(ModelAndView modelAndView) throws SQLException {
+        DAOFactory daoFactory = new DAOFactory();
 
-        System.out.println("S-a afisat pagina patientHomePage.hmtl! Este conectat clientul cu email-ul:" + email);
-        return "patientDashboard";
+        DoctorDAO doctorDAO = new DoctorDAO(daoFactory.getConnection());
+
+        String doctorsJson = doctorDAO.selectJSON();
+        modelAndView.addObject("json", doctorsJson);
+        modelAndView.setViewName("patientDashboard");
+
+        System.out.println("S-a afisat pagina patientHomePage.hmtl! Este conectat clientul cu email-ul:" + session.getAttribute("email"));
+
+        return modelAndView;
     }
 }
