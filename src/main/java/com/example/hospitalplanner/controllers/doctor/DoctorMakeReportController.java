46c438597c53ca2bf7ffd65b2eafc4a1c7ed3dd7
Maria Onofrei <94899775+onfma@users.noreply.github.com>
2023-05-21 19:25:08 +0300

    created doctor make report

---
+++ src/main/java/com/example/hospitalplanner/controllers/doctor/DoctorMakeReportController.java
@@ -0,0 +1,20 @@
+package com.example.hospitalplanner.controllers.doctor;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/doctorMakeReport")
+public class DoctorMakeReportController {
+    @Autowired
+    private HttpSession session;
+
+    @GetMapping
+    public String showDoctorDashboard() {
+        System.out.println("S-a afisat pagina doctorMakeReport.html! E conectat: " + session.getAttribute("email"));
+        return "doctorMakeReport";
+    }
+}
