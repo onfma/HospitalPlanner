fd6ee4fc99a82394dc94cce15fa1899e04d475c6
claudiuchichirau <chichirau.claudiu@yahoo.ro>
2023-05-21 14:34:54 +0300

    configure DoctorViewAccount (add real data from DB)

--- src/main/java/com/example/hospitalplanner/controllers/doctor/DoctorViewAccountController.java
+++ src/main/java/com/example/hospitalplanner/controllers/doctor/DoctorViewAccountController.java
@@ -1,11 +1,19 @@
 package com.example.hospitalplanner.controllers.doctor;
 
 
+import com.example.hospitalplanner.database.DAO.DoctorDAO;
+import com.example.hospitalplanner.database.DAO.PatientDAO;
+import com.example.hospitalplanner.database.DAOFactory;
+import com.example.hospitalplanner.entities.person.Doctor;
+import com.example.hospitalplanner.entities.person.Patient;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.sql.SQLException;
 
 @Controller
 @RequestMapping("/doctorViewAccount")
@@ -14,8 +22,28 @@ public class DoctorViewAccountController {
     private HttpSession session;
 
     @GetMapping
-    public String showDoctorDashboard() {
+    public String showDoctorDashboard(Model model) throws SQLException {
         System.out.println("S-a afisat pagina doctorDashboard.html.hmtl!");
+
+        DAOFactory daoFactory = new DAOFactory();
+        DoctorDAO doctorDAO = new DoctorDAO(daoFactory.getConnection());
+
+        String personEmail = (String) session.getAttribute("email");
+        long cnp = doctorDAO.getCNP(personEmail);
+
+        Doctor doctor = new Doctor();
+
+        doctor.setCnp(cnp);
+        doctor.setFirstName(doctorDAO.getFirstName(cnp));
+        doctor.setLastName(doctorDAO.getLastName(cnp));
+        doctor.setGender(doctorDAO.getGender(cnp));
+        doctor.setPhoneNumber(doctorDAO.getPhoneNumber(cnp));
+        doctor.setAddress(doctorDAO.getAddress(cnp));
+        doctor.setEmail(doctorDAO.getEmail(cnp));
+
+        // Add patient in model
+        model.addAttribute("doctor", doctor);
+
         return "doctorViewAccount";
     }
 }
