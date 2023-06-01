ded071bb7498809189382134f051378c50c432b1
Maria Onofrei <94899775+onfma@users.noreply.github.com>
2023-06-01 13:34:01 +0300

    i want a ckean recovery in caz ca o dau de gard cu erorile astea

--- src/main/java/com/example/hospitalplanner/HospitalPlannerApplication.java
+++ src/main/java/com/example/hospitalplanner/HospitalPlannerApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.example.hospitalplanner")
+//@ComponentScan(basePackages = "com.example.hospitalplanner")
 @RestController
 public class HospitalPlannerApplication {
 //http://localhost:9087/
